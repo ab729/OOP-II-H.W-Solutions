6a68e6e317f3b83132785121aaa18b51f3e6fe6c
Ahmed Barazenji <99739001+ab729@users.noreply.github.com>
2022-11-27 21:34:50 +0300

    Update 9.7.java

--- Chapter 9/9.7.java
+++ Chapter 9/9.7.java
@@ -1,2 +1,18 @@
 //Q1
-DateManager.printTodaysDate();
+  DateManager.printTodaysDate();
+//Q2
+  DataTransmitter.sendSignal();
+//Q3
+eurasiaSales = Arithmetic.add(euroSales,asiaSales);
+//Q4
+cubeVolume = IMath.toThePowerOf(cubeSide, 3);
+//Q5
+DataTransmitter.sendDouble(x);
+//Q6
+Logger.printLarger(sales1, sales2);
+//Q7
+DataTransmitter.sendTwo(15.955,133);
+//Q8
+DataTransmitter.sendNumber(5);
+//Q9
+Logger.printErrorDescription(14);
