421601eb2765a4042278eacc1f0b4d8c801308a3
Ahmed Barazenji <99739001+ab729@users.noreply.github.com>
2022-11-27 21:30:22 +0300

    Create 9.5.java

---
+++ Chapter 9/9.5.java
@@ -0,0 +1,37 @@
+//Q1
+File myFile;
+//Q2
+Menu myMenu;
+//Q3
+myAC.turnOff();
+//Q4
+officeAC = new AirConditioner();
+officeAC.turnOn();
+officeAC.setTemp(69);
+//Q5
+dataTransmitter.sendNumber(5);
+//Q6
+myAC.setTemp(72);
+//Q7
+eurasiaSales = arithmetic.add(euroSales, asiaSales);
+//Q8
+dataTransmitter.sendDouble(x);
+//Q9
+Integer myInt = new Integer(75);
+//Q10
+currentTemp = myAC.getTemp();
+//Q11
+dateManager.printTodaysDate();
+//Q12
+String myString2 = new String();
+myString = myString2;
+//Q13
+status = myAC.isOn();
+//Q14
+logger.printErrorDescription(14);
+//Q15
+myAC.turnOn();
+//Q16
+//Try your best in this one
+//Q17
+dataTransmitter.sendObject(john_doe);
